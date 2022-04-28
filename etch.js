06551e9929ff5dcdec1231088f604c4d860eb7e0
vygantas <vygisltu@outlook.com>
2022-04-28 18:43:56 +0300

    working square with ra ndom colors

--- etch.js
+++ etch.js
@@ -1,3 +1,31 @@
-const container = document.querySelector('container')
-const div = document.createElement('div')
-div.style['backgroundColor: blue']
+
+const container = document.querySelector('#container')
+const content = document.createElement('content')
+
+function createGrid(size){
+
+for (let i=1; i <=size; i++){
+    let randomcolor = Math.floor(Math.random()*16777215).toString(16);
+    let board = document.createElement('div')
+    board.classList.add('box')
+
+
+    board.addEventListener('mouseenter', () =>
+board.style.backgroundColor = '#' + randomcolor
+)   
+   board.addEventListener('mouseleave', () =>
+board.style.backgroundColor = 'white'
+)   
+container.append(board)
+} 
+
+  
+    
+ 
+ 
+
+}
+
+console.log(createGrid(50))
+
+
